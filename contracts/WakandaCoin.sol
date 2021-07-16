d41b420949a42e35eecc247131c6ea1d8a684845
wakanda admin <admin@wakanda-coin.com>
2021-07-16 18:19:00 -0400

    Changed to using variables for total supply and other values

--- contracts/WakandaCoin.sol
+++ contracts/WakandaCoin.sol
@@ -1,14 +1,24 @@
 pragma solidity ^0.5.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
-import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v2.5.1/contracts/token/ERC20/ERC20.sol";
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v2.5.1/contracts/token/ERC20/ERC20Burnable.sol";
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v2.5.1/contracts/token/ERC20/ERC20Pausable.sol";
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v2.5.1/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v2.5.1/contracts/ownership/Ownable.sol";
 
 contract WakandaCoin is ERC20, ERC20Detailed, ERC20Burnable, ERC20Pausable, Ownable{
-	constructor() public ERC20Detailed("WakandaCoin", "WAKC", 18) {
-		_mint(msg.sender,1000000000000000 * (10 ** uint256(decimals())));
-	}
+    
+    string internal constant _name = "WakandaCoin";
+    string internal constant _symbol = "WAKC";
+    uint8 internal constant _decimal = 18;
+    
+    uint256 internal constant tokenUnit = 10**18;
+    uint256 internal constant oneQuadrillion = 10**15;
+    uint256 internal constant oneBillion = 10**9;
+    uint256 internal constant maxSupply = oneQuadrillion * tokenUnit;   
+    
+    
+    constructor() public ERC20Detailed(_name, _symbol,_decimal) {
+        _mint(msg.sender,maxSupply);
+    }
 }
-
